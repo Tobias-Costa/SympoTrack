1ea0a5ff52538f9cf3ebb9a4ec2564e557d7c40d
Tobias <tobiascosta064@gmail.com>
2026-05-15 15:10:07 -0300

    feat: database tables added to admin panel

--- sympo_track_app/admin.py
+++ sympo_track_app/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
-from .models import User
+from .models import (
+    User,
+    Language,
+    Country,
+    State,
+    City,
+    EventAddress,
+    EventCategories,
+    Event,
+    EventCategoryRel,
+    EventStagesType,
+    EventStage,
+    EventPricing,
+    EventRole,
+    ManagementGroup,
+    ManagementGroupMember,
+    EventSubscription,
+    UserStageRequirement,
+    CancellationReason,
+    NotificationsSubject,
+    Notification,
+)
 
 # Register your models here.
 
@@ -39,5 +60,53 @@ class CustomUserAdmin(UserAdmin):
     )
 
     ordering = ('email',)
-    
+
+# ===========================================================
+# USUÁRIOS
+# ===========================================================
+
 admin.site.register(User, CustomUserAdmin)
+
+# ===========================================================
+# LOCALIZAÇÃO
+# ===========================================================
+
+admin.site.register(Language)
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(City)
+admin.site.register(EventAddress)
+
+# ===========================================================
+# EVENTOS
+# ===========================================================
+
+admin.site.register(EventCategories)
+admin.site.register(Event)
+admin.site.register(EventCategoryRel)
+admin.site.register(EventStagesType)
+admin.site.register(EventStage)
+admin.site.register(EventPricing)
+
+# ===========================================================
+# GESTÃO
+# ===========================================================
+
+admin.site.register(EventRole)
+admin.site.register(ManagementGroup)
+admin.site.register(ManagementGroupMember)
+
+# ===========================================================
+# INSCRIÇÕES
+# ===========================================================
+
+admin.site.register(EventSubscription)
+admin.site.register(UserStageRequirement)
+admin.site.register(CancellationReason)
+
+# ===========================================================
+# NOTIFICAÇÕES
+# ===========================================================
+
+admin.site.register(NotificationsSubject)
+admin.site.register(Notification)
