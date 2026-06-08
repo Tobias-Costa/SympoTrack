dcca7d83b8b94b9ea307efd1dab5801529a3522a
Tobias <tobiascosta064@gmail.com>
2026-06-08 15:22:14 -0300

    feat: register EventAddress function implemented

--- sympo_track_app/urls.py
+++ sympo_track_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("register/event", views.register_event, name="register_event"),
     path("register/language", views.register_language, name="register_language"),
+    path("register/address", views.register_address, name="register_address"),
+    path("register/address/city", views.register_city, name="register_city"),
 ]
--- sympo_track_app/views.py
+++ sympo_track_app/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db import DatabaseError
-from .models import Language
+from .models import Language, State, City, EventAddress
 
 # Create your views here.
 
@@ -15,8 +15,11 @@ def home(request):
 def register_event(request):
      # Retorna todas as linguagens cadastradas no DB
     languages = Language.objects.all().order_by("language")
+    address = EventAddress.objects.all().order_by("place_name")
 
-    return render(request, "register_event.html", {"languages":languages,})
+    return render(request, "register_event.html", {"languages":languages, "address_options":address})
+
+# ------------------------ REGISTER VIEWS DE CAMPOS ADICIONAIS DO REGISTER EVENT ------------------------
 
 @login_required
 def register_language(request):
@@ -29,10 +32,58 @@ def register_language(request):
             messages.success(request, "Idioma adicionado.")
         except DatabaseError as e:
             # Captura o erro do banco de dados e avisa o usuário sem derrubar o sistema
-            messages.error(request, f"Erro ao salvar o idioma: {e}")
+            messages.error(request, f"Erro ao salvar o idioma")
         except Exception as e:
             # Captura qualquer outro erro inesperado
-            messages.error(request, f"Ocorreu um erro inesperado: {e}")
+            messages.error(request, f"Ocorreu um erro inesperado")
     
     return render(request, "register_language.html")
+
+@login_required
+def register_address(request):
+    if request.method == "POST":
+        try:
+            place_name=request.POST.get('place_name')
+            cep=request.POST.get('cep')
+            street=request.POST.get('street')
+            number=request.POST.get('number')
+            complement=request.POST.get('complement')
+            neighborhood=request.POST.get('neighborhood')
+            city = get_object_or_404(City, id=request.POST.get('city'))
+
+            # Salvando endereço do formulário no model EventAddress
+            event_addr = EventAddress(place_name=place_name, street=street, number=number, complement=complement, neighborhood=neighborhood, cep=cep, city=city)
+            event_addr.save()
+            # Atualiza buffer de messages
+            messages.success(request, "Endereço adicionado.")
+        except DatabaseError as e:
+            # Captura o erro do banco de dados e avisa o usuário sem derrubar o sistema
+            messages.error(request, f"Erro ao salvar o endereço")
+        except Exception as e:
+            # Captura qualquer outro erro inesperado
+            messages.error(request, f"Ocorreu um erro inesperado")
+
+    cities = City.objects.all().order_by("name")
+    return render(request, "register_address.html", {"cities":cities})
+
+
+@login_required
+def register_city(request):
+    if request.method == "POST":
+        try:
+            # Salvando cidade do formulário no model City
+            state_id = get_object_or_404(State, id=request.POST.get('state'))
+            city = City(name=request.POST.get('name'), state=state_id)
+            city.save()
+            # Atualiza buffer de messages
+            messages.success(request, "Cidade adicionada.")
+        except DatabaseError as e:
+            # Captura o erro do banco de dados e avisa o usuário sem derrubar o sistema
+            messages.error(request, f"Erro ao salvar a cidade")
+        except Exception as e:
+            # Captura qualquer outro erro inesperado
+            messages.error(request, f"Ocorreu um erro inesperado")
+
+    states = State.objects.all().order_by('name')
+    return render(request, "register_city.html", {'states':states})
     
