fb39c4fd1d2584447d94217047bdb930988991bf
Tobias <tobiascosta064@gmail.com>
2026-06-08 14:22:23 -0300

    feat: register language function implemented

--- sympo_track_app/urls.py
+++ sympo_track_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("register/event", views.register_event, name="register_event"),
+    path("register/language", views.register_language, name="register_language"),
 ]
--- sympo_track_app/views.py
+++ sympo_track_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.db import DatabaseError
+from .models import Language
 
 # Create your views here.
 
@@ -10,4 +13,26 @@ def home(request):
 
 @login_required
 def register_event(request):
-    return render(request, "register_event.html")
+     # Retorna todas as linguagens cadastradas no DB
+    languages = Language.objects.all().order_by("language")
+
+    return render(request, "register_event.html", {"languages":languages,})
+
+@login_required
+def register_language(request):
+    if request.method == "POST":
+        try:
+            # Salvando linguagem do formulário no model Language
+            language = Language(language=request.POST.get('language'))
+            language.save()
+            # Atualiza buffer de messages
+            messages.success(request, "Idioma adicionado.")
+        except DatabaseError as e:
+            # Captura o erro do banco de dados e avisa o usuário sem derrubar o sistema
+            messages.error(request, f"Erro ao salvar o idioma: {e}")
+        except Exception as e:
+            # Captura qualquer outro erro inesperado
+            messages.error(request, f"Ocorreu um erro inesperado: {e}")
+    
+    return render(request, "register_language.html")
+    
