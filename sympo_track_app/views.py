1a8a25c8bedb84090bb20eba817ebc65c83bb41a
Tobias <tobiascosta064@gmail.com>
2026-06-24 15:31:57 -0300

    feat: autocomplete map visualization done

--- sympo_track_app/views.py
+++ sympo_track_app/views.py
@@ -19,7 +19,7 @@ def register_event(request):
     addresses = EventAddress.objects.all().order_by("place_name")
     categories = EventCategories.objects.all().order_by("name")
 
-    return render(request, "register_event.html", {"languages":languages, "addresses_options":addresses, "category_options":categories, })
+    return render(request, "register_event.html", {"languages":languages, "addresses_options":addresses, "category_options":categories, "geoapify_key":settings.GEOAPIFY_API_KEY})
 
 # ------------------------ REGISTER VIEWS DE CAMPOS ADICIONAIS DO REGISTER EVENT ------------------------
 
@@ -41,73 +41,73 @@ def register_language(request):
     
     return render(request, "register_language.html")
 
-@login_required
-def register_address(request):
-    if request.method == "POST":
-        country_name = request.POST.get("country_name")
-        country_abbr = request.POST.get("country_abbr")
+# @login_required
+# def register_address(request):
+#     if request.method == "POST":
+#         country_name = request.POST.get("country_name")
+#         country_abbr = request.POST.get("country_abbr")
 
-        state_name = request.POST.get("state_name")
-        state_uf = request.POST.get("state_uf")
+#         state_name = request.POST.get("state_name")
+#         state_uf = request.POST.get("state_uf")
 
-        city_name = request.POST.get("city_name")
+#         city_name = request.POST.get("city_name")
 
-        if not (country_name and state_name and city_name):
-            messages.error(
-                request,
-                "Selecione um endereço válido no mapa."
-            )
-            return redirect("cadastrar_endereco_evento")
+#         if not (country_name and state_name and city_name):
+#             messages.error(
+#                 request,
+#                 "Selecione um endereço válido no mapa."
+#             )
+#             return redirect("cadastrar_endereco_evento")
 
-        try:
+#         try:
 
-            country, _ = Country.objects.get_or_create(
-                name=country_name,
-                defaults={
-                    "abbr": country_abbr or country_name[:10]
-                }
-            )
+#             country, _ = Country.objects.get_or_create(
+#                 name=country_name,
+#                 defaults={
+#                     "abbr": country_abbr or country_name[:10]
+#                 }
+#             )
 
-            state, _ = State.objects.get_or_create(
-                name=state_name,
-                country=country,
-                defaults={
-                    "uf": state_uf or state_name[:10]
-                }
-            )
+#             state, _ = State.objects.get_or_create(
+#                 name=state_name,
+#                 country=country,
+#                 defaults={
+#                     "uf": state_uf or state_name[:10]
+#                 }
+#             )
 
-            city, _ = City.objects.get_or_create(
-                name=city_name,
-                state=state
-            )
+#             city, _ = City.objects.get_or_create(
+#                 name=city_name,
+#                 state=state
+#             )
 
-            EventAddress.objects.create(
-                place_name=request.POST.get("place_name"),
-                street=request.POST.get("street"),
-                number=request.POST.get("number") or None,
-                complement=request.POST.get("complement") or None,
-                neighborhood=request.POST.get("neighborhood") or None,
-                cep=request.POST.get("cep") or None,
-                city=city
-            )
+#             EventAddress.objects.create(
+#                 place_name=request.POST.get("place_name"),
+#                 street=request.POST.get("street"),
+#                 number=request.POST.get("number") or None,
+#                 complement=request.POST.get("complement") or None,
+#                 neighborhood=request.POST.get("neighborhood") or None,
+#                 cep=request.POST.get("cep") or None,
+#                 city=city
+#             )
 
-            messages.success(
-                request,
-                "Endereço cadastrado com sucesso."
-            )
+#             messages.success(
+#                 request,
+#                 "Endereço cadastrado com sucesso."
+#             )
 
-            return redirect("lista_eventos")
+#             return redirect("lista_eventos")
 
-        except Exception as e:
+#         except Exception as e:
 
-            messages.error(
-                request,
-                f"Erro ao salvar endereço: {str(e)}"
-            )
+#             messages.error(
+#                 request,
+#                 f"Erro ao salvar endereço: {str(e)}"
+#             )
 
-            return redirect("cadastrar_endereco_evento")
+#             return redirect("cadastrar_endereco_evento")
         
-    return render(request, "register_address.html", {"geoapify_key":settings.GEOAPIFY_API_KEY})
+#     return render(request, "register_address.html")
 
 
 # @login_required
