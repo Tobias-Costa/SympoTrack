1da88931568ad0d9ca6206c0d7fd48f9a4933c44
Tobias-Costa <tobiascosta064@gmail.com>
2026-07-16 14:51:59 -0300

    feat: email settings update

--- sympo_core/settings.py
+++ sympo_core/settings.py
@@ -139,16 +139,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# settings.py para TESTE (exibe no terminal)
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-# settings.py para PRODUÇÃO(USAR OS.GETENV)
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = '://gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'seu-email@gmail.com'
-# EMAIL_HOST_PASSWORD = 'sua-senha-de-aplicativo' # Não é a senha normal!
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
